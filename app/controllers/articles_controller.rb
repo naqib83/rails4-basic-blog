f5b436f9898a5c24685e70a3c4cfa0956c0e3284
Naqib Hossain <naqib83@gmail.com>
2018-03-05 14:04:13 -0600

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,72 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page:4)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    #@article = Article.find(params[:id])
+  end
+
+  def create
+    #debugger
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    #@article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success]  = "Article successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
+
+  def show
+    #@article = Article.find(params[:id])
+  end
+
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :description, :testcolumn, category_ids:[])
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @article.user and !current_user.admin?
+        flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
+  end
+
+
+end
